b342ad1dd41d98b0d28285be52cc459d80e13fc5
Andrew Messier <andrewgmessier@gmail.com>
2020-03-28 13:46:27 -0400

    Initial commit

---
+++ server/HttpOfTheDay.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace agm.OfTheDay
+{
+	public static class HttpOfTheDay
+	{
+		[FunctionName("HttpOfTheDay")]
+		public static async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+			ILogger log)
+		{
+			log.LogInformation("C# HTTP trigger function processed a request.");
+
+			string name = req.Query["name"];
+
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			dynamic data = JsonConvert.DeserializeObject(requestBody);
+			name = name ?? data?.name;
+
+			string responseMessage = string.IsNullOrEmpty(name) ? "Hello, World!" : $"Hello, {name}!";
+
+			return new OkObjectResult(responseMessage);
+		}
+	}
+}
