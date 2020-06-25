97a4a2db3456e4357c93665818608d560cf93888
Andrew Messier <andrewgmessier@gmail.com>
2020-06-25 13:54:41 -0400

    Add shared project with tsconfig. Also add LICENSE

---
+++ projects/shared/src/posts/index.ts
@@ -0,0 +1,48 @@
+export interface OfTheDayData {
+	keyVal: KeyVal;
+
+	checklistDone: string[];
+	checklistToDo: string[];
+
+	dailyRecords: DailyRecord[];
+	musicRecords: MusicRecord[];
+}
+
+export interface KeyVal {
+	importantInformation: string;
+	badInformation: string;
+	startDate: string;
+	workingOn: string;
+	lookingForwardTo: string;
+}
+
+export interface SharedDayRecord {
+	day: string;
+	dayAsText: string;
+	dayNumber: number;
+	specialEvent: string;
+}
+
+export interface DailyRecord extends SharedDayRecord {
+	location: string;
+	note: string;
+	schedule: string;
+	quote: string;
+	quoteBy: string;
+	youTubeLink: string;
+	youTubeLinkTitle: string;
+	youTubeLinkDescription: string;
+	howDidItGo: string;
+}
+
+export interface MusicRecord extends SharedDayRecord {
+	isFavorite: boolean;
+	title: string;
+	artist: string;
+	spotifyLink: string;
+	youTubeLink: string;
+	isYouTubePreferred: boolean;
+	geniusLink: string;
+	description: string;
+	quote: string;
+}
